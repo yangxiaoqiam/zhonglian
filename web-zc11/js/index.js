a3cb19e4502e838901a43d664fb0d808f05dad42
xuanhui <541951523@qq.com>
2017-05-18 14:53:51 +0800

    中联

---
+++ web-zc11/js/index.js
@@ -0,0 +1,27 @@
+﻿  $(function(){
+
+  	  $(".d-section6 .d-bg62").mouseover(function(){
+         $(this).addClass("open");
+      })
+      $(".d-section6 .d-bg62").mouseout(function(){
+
+         $(this).removeClass("open");
+      })
+
+       $(".d-section6 .d-bg63").mouseover(function(){
+         $(this).addClass("open");
+      })
+      $(".d-section6 .d-bg63").mouseout(function(){
+
+         $(this).removeClass("open");
+      })
+
+
+
+
+     
+       var zhheight1=window.screen.height;
+       $(".d-section8box8").css({height:zhheight1});
+       $(".d-section9box").css({height:zhheight1});
+        $(".d-section10box").css({height:zhheight1});
+  })
